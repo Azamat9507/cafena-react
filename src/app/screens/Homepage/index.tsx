77ae3cab516fe6385d5e218864a66a957ff84c78
Azamat9507 <solijonov.azamat9507@gmail.com>
2023-03-25 07:31:00 +0900

    feat: homePage screen slice & selector logic creation

--- src/app/screens/Homepage/index.tsx
+++ src/app/screens/Homepage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Container } from "@mui/material";
 import { Statistics } from './statistics';
 import { TopRestaurants } from './topRestaurants';
@@ -8,16 +8,15 @@ import { Advertisements } from './advertisements';
 import { Events } from './events';
 import { Recommendations } from './recommendations';
 import "../../../css/home.css";
-import { useEffect } from 'react';
-
 
 export function Homepage() {
-  useEffect(() => {
-    console.log("componentDidmount => Data fetch");
 
-    return () => {
-      console.log("componentWillUnmount process");
-    };
+  //selector: store => data
+
+  useEffect(() => {
+    // backend data request => data
+
+    // slice: data => store
   }, []);
   
   return (  
